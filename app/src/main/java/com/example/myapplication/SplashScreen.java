892d074dd414f9b4a5ad9c7e60ffbce188cf8625
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2020-04-25 23:32:32 +0530

    added splash screen

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        int SPLASH_TIME = 2000;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mySuperIntent);
+
+                //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+                finish();
+
+            }
+        }, SPLASH_TIME);
+    }
+}
