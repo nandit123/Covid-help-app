0c4b43c2c29363e7817320de9969edd5b9f96cec
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2020-04-26 14:58:41 +0530

    added map functionality and styling

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -18,11 +20,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.Task;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -49,9 +56,8 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("38299970165-l5hghb1ds0b8rgnt66vjcd1ekip07eus.apps.googleusercontent.com")
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
                 .requestServerAuthCode("38299970165-l5hghb1ds0b8rgnt66vjcd1ekip07eus.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
@@ -82,14 +88,14 @@ public class LoginActivity extends AppCompatActivity {
 
     private String postRequest(final String idToken) {
         RequestQueue requestQueue= Volley.newRequestQueue(LoginActivity.this);
-        String url="http://127.0.0.1:8001/api/v1/google/verifyAccessToken";
+        String url="http://35.222.249.89:8001/api/v1/google/verifyAccessToken";
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 //let's parse json data
                 try {
-                    //JSONObject jsonObject = new JSONObject(response);
-                    //String access_token = jsonObject.getJSONObject("pageInfo").getString("pageName");
+                    TextView text = (TextView) findViewById(R.id.token_view);
+                    text.setText(response);
                     Log.e("error","Post Data : Success");
                 }
                 catch (Exception e){
@@ -123,20 +129,106 @@ public class LoginActivity extends AppCompatActivity {
         return null;
     }
 
+
+    private String get_access_token(final String auth_code){
+        RequestQueue requestQueue= Volley.newRequestQueue(LoginActivity.this);
+        String url="https://oauth2.googleapis.com/token";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //let's parse json data
+                try {
+                    TextView text = (TextView) findViewById(R.id.token_view);
+                    text.setText(response);
+                    Log.e("Error","Resp"+response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("error", "Post Data : Response Failed");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("code", auth_code);
+                params.put("client_id", "38299970165-l5hghb1ds0b8rgnt66vjcd1ekip07eus.apps.googleusercontent.com");
+                params.put("client_secret", "7Gxal5CEbEnuzKMh-SNVMR_F");
+                params.put("redirect_uri", "http://localhost");
+                params.put("grant_type", "authorization_code");
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                //params.put("Content-Type","application/x-www-form-urlencoded");
+                params.put("Accept", "application/json");
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+        //Log.e("error","string req"+ stringRequest[0]);
+        return null;
+    }
+
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            String idToken = account.getIdToken();
-            String id = account.getId();
-            String au = account.getServerAuthCode();
-            Log.e("error","Token :"+ idToken);
-            Log.e("error","ID :"+ id);
-            Log.e("error","auth :"+ au);
+            String auth_code = account.getServerAuthCode();
+            get_access_token(auth_code);
+            final TextView text = (TextView) findViewById(R.id.token_view);
+
+
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    String access_token = text.getText().toString();
+                    String[] arrOfStr = access_token.split(":", 3);
+                    String arrOfStr2 = arrOfStr[1].split(",", 2)[0];
+                    String arrOfStr3 = arrOfStr2.substring(2, arrOfStr2.length()-1);
+                    Log.e("error","done i guess :"+ arrOfStr3);
+
+                    postRequest(arrOfStr3);
+
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            final TextView text = (TextView) findViewById(R.id.token_view);
+                            String access_token = text.getText().toString();
+
+                            JSONObject reader = null;
+                            try {
+                                reader = new JSONObject(access_token);
+                                JSONObject sys  = reader.getJSONObject("data");
+                                String main_token = sys.getString("access_token");
+                                Log.e("error", "Final Resp : ------------------------------" + main_token);
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("main_token", main_token);
+                                startActivity(intent);
+
+                            } catch (JSONException e) {
+                                Log.e("error", "Final Resp : Fail--------------------------" );
+                                e.printStackTrace();
+                            }
+                        }
+                    }, 2000);
+                }
+            }, 3000);
+
+            Log.e("error","auth :"+ auth_code);
+            //Log.e("error","got token :"+ access_token);
             //Log.e("error","Token :"+ idToken);
-            String token = postRequest(idToken);
+            //String token = postRequest(access_token);
             // Signed in successfully, show authenticated UI.
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
+
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,11 +19,15 @@ import com.google.android.gms.maps.model.Dash;
 import com.google.android.gms.maps.model.Dot;
 import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PatternItem;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -78,6 +84,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final PatternItem DASH = new Dash(PATTERN_DASH_LENGTH_PX);
     private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
 
+    // Create a stroke pattern of a gap followed by a dot.
+    private static final List<PatternItem> PATTERN_POLYLINE_DOTTED = Arrays.asList(GAP, DOT);
+
+    // Create a stroke pattern of a gap followed by a dash.
+    private static final List<PatternItem> PATTERN_POLYGON_ALPHA = Arrays.asList(GAP, DASH);
+
+    // Create a stroke pattern of a dot followed by a gap, a dash, and another gap.
+    private static final List<PatternItem> PATTERN_POLYGON_BETA =
+            Arrays.asList(DOT, GAP, DASH, GAP);
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -89,10 +105,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e("no success", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("no style", "Can't find style. Error: ", e);
+        }
+
+
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng amritsar = new LatLng(31.633980, 74.872261);
+        double LL[][]  = {
+                {31.622581,74.851411},
+                {31.613517,74.871165},
+                {31.620242,74.898306},
+                {31.646696,74.911189},
+                {31.659263,74.884220},
+                {31.657199,74.869437}
+        };
+
+        double LL2[][]  = {
+
+                {31.638640,74.884170},
+                {31.604051,74.846131},
+//                {31.610500,74.841130},
+                {31.623895,74.827881}
+//                {31.610133,74.831461}
+        };
+
         mMap.addMarker(new MarkerOptions().position(amritsar).title("Amritsar").snippet("Save Punjab NGO Delivered 4 days back"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(amritsar));
 
@@ -100,35 +149,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Zoom out to zoom level 10, animating with a duration of 2 seconds.
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(13), 2000, null);
 
+        LatLng a1 = new LatLng(31.622581,74.851411);
+        LatLng a2 = new LatLng(31.613517,74.871165);
+        LatLng a3 = new LatLng(31.620242,74.898306);
+        LatLng a4 = new LatLng(31.646696,74.911189);
+        LatLng a5 = new LatLng(31.659263,74.884220);
+        LatLng a6 = new LatLng(31.657199,74.869437);
+
+        LatLng a7 = new LatLng(31.638640,74.884170);
+        LatLng a8 = new LatLng(31.604051,74.846131);
+        LatLng a9 = new LatLng(31.623895,74.827881);
+
+        mMap.addMarker(new MarkerOptions().position(a1).title("Akash NGO").snippet("Food-1800, Medical-790 (Today)"));
+        mMap.addMarker(new MarkerOptions().position(a2).title("Amritsar Helpline").snippet("Food-3000, Medical-1400 (1 day ago)"));
+        mMap.addMarker(new MarkerOptions().position(a3).title("Save People NGO").snippet("Food-2300, Medical-2500 (1 day ago)"));
+        mMap.addMarker(new MarkerOptions().position(a4).title("Veer Group").snippet("Food-1900, Medical-1790 (2 day ago)"));
+        mMap.addMarker(new MarkerOptions().position(a5).title("UNE Society").snippet("Food-2800, Medical-2790 (3 day ago)"));
+        mMap.addMarker(new MarkerOptions().position(a6).title("Goodwill NGO").snippet("Food-4500, Medical-4652 (4 day ago)"));
+
+
+        mMap.addMarker(new MarkerOptions().position(a7).title("People People").snippet("Food-3999, Medical-4888 (8 day ago)"));
+        mMap.addMarker(new MarkerOptions().position(a8).title("VSO Society").snippet("Food-2800, Medical-2790 (7 day ago)"));
+        mMap.addMarker(new MarkerOptions().position(a9).title("Together We").snippet("Food-4500, Medical-4652 (8 day ago)"));
         // Instantiates a new Polygon object and adds points to define a rectangle
-        PolygonOptions rectOptions = new PolygonOptions()
-                .add(new LatLng(31.671061,74.894678),
-                        new LatLng(31.614943,74.946837),
-                        new LatLng(31.581022,74.858889),
-                        new LatLng(31.622544,74.773690),
-                        new LatLng(31.671061,74.894678));
-
-        // Get back the mutable Polygon
-
-        Polygon polygon = googleMap.addPolygon(rectOptions);
+//        PolygonOptions rectOptions = new PolygonOptions()
+//                .add(new LatLng(31.671061,74.894678),
+//                        new LatLng(31.614943,74.946837),
+//                        new LatLng(31.581022,74.858889),
+//                        new LatLng(31.622544,74.773690),
+//                        new LatLng(31.671061,74.894678));
+//
+//        // Get back the mutable Polygon
+//
+//        Polygon polygon = googleMap.addPolygon(rectOptions);
         // Instantiates a new CircleOptions object and defines the center and radius
+
+        Polygon polygon1 = googleMap.addPolygon(new PolygonOptions()
+                .clickable(true).
+                        add(new LatLng(31.671061,74.894678),
+                                new LatLng(31.614943,74.946837),
+                                new LatLng(31.581022,74.858889),
+                                new LatLng(31.622544,74.773690),
+                                new LatLng(31.671061,74.894678)));
+        polygon1.setStrokeWidth(10);
+        polygon1.setStrokePattern(PATTERN_POLYLINE_DOTTED);
+
         CircleOptions circleOptions = new CircleOptions()
                 .center(new LatLng(31.633980, 74.872261))
                 .radius(1000); // In meters
 
         // Get back the mutable Circle
         Circle circle = googleMap.addCircle(circleOptions);
-
-
-        Polygon polygon1 = googleMap.addPolygon(new PolygonOptions()
-                .clickable(true)
-                .add(
-                        new LatLng(-27.457, 153.040),
-                        new LatLng(-33.852, 151.211),
-                        new LatLng(-37.813, 144.962),
-                        new LatLng(-34.928, 138.599)));
-//        polygon1.setFillColor(COLOR_GREEN_ARGB);
-        circle.setFillColor(COLOR_GREEN_ARGB);
+        circle.setFillColor(0x5500ff00);
         circle.setStrokeColor(0x00000000);
+
+        for(int i=0;i<LL.length;i++)
+        {
+            circleOptions = new CircleOptions()
+                    .center(new LatLng(LL[i][0], LL[i][1]))
+                    .radius(1000);
+            circle = googleMap.addCircle(circleOptions);
+            circle.setFillColor(0x5500ff00);
+            circle.setStrokeColor(0x00000000);
+        }
+
+        for(int i=0;i<LL2.length;i++)
+        {
+            circleOptions = new CircleOptions()
+                    .center(new LatLng(LL2[i][0], LL2[i][1]))
+                    .radius(1000);
+            circle = googleMap.addCircle(circleOptions);
+            circle.setFillColor(0x33ff0000);
+            circle.setStrokeColor(0x00000000);
+        }
     }
 }
